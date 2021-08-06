b63eabbbcceff9c9de0dd9ec698e1989ccae826f
Fazal9999 <faalurrehman442@gmail.com>
2021-08-06 13:03:08 +0500

    Location

--- app/src/main/java/com/example/multifx/MainActivity.kt
+++ app/src/main/java/com/example/multifx/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.multifx
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.webkit.GeolocationPermissions
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    private lateinit var context: Context
     lateinit var activity: Activity
     lateinit var progDailog: ProgressDialog
     @SuppressLint("SetJavaScriptEnabled")
@@ -21,14 +30,30 @@ class MainActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
         setContentView(R.layout.activity_main)
+
+        statusCheck()
         webView = findViewById(R.id.webView)
         val url = "https://maxelint.com/"
         activity = this;
+        context=this
         progDailog = ProgressDialog.show(activity, "Loading", "Please wait...", true);
         progDailog.setCancelable(false);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String,
+                callback: GeolocationPermissions.Callback
+            ) {
+                callback.invoke(origin, true, false)
+            }
+        }
+        webView.getSettings().setGeolocationDatabasePath(context.getFilesDir().getPath());
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 progDailog.show()
@@ -40,7 +65,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
         webView.loadUrl(url)
-    }}
+    }
+
+    fun statusCheck() {
+        val manager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps()
+        }
+    }
+
+    private fun buildAlertMessageNoGps() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+            .setCancelable(false)
+            .setPositiveButton("Yes",
+                DialogInterface.OnClickListener { dialog, id -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) })
+            .setNegativeButton("No",
+                DialogInterface.OnClickListener { dialog, id -> dialog.cancel()
+                    this.finish();
+                    System.exit(0);
+                })
+        val alert: AlertDialog = builder.create()
+        alert.show()
+    }
+}
 
 
 
