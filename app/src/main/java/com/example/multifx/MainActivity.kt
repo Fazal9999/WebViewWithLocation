eaf46bffb4b54b58c9a9e3aa033e15d1f378952f
Fazal9999 <faalurrehman442@gmail.com>
2021-08-06 15:20:38 +0500

    final

--- app/src/main/java/com/example/multifx/MainActivity.kt
+++ app/src/main/java/com/example/multifx/MainActivity.kt
@@ -4,17 +4,19 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
+import android.content.IntentSender.SendIntentException
 import android.location.LocationManager
 import android.os.Bundle
-import android.provider.Settings
 import android.webkit.GeolocationPermissions
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +24,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var context: Context
     lateinit var activity: Activity
     lateinit var progDailog: ProgressDialog
+   lateinit var locationManager:LocationManager
+    fun isGpsEnabled(): Boolean {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    //Defining constant request code, add in your activity class
+    private val REQUEST_CHECK_SETTINGS = 111
+
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
         setContentView(R.layout.activity_main)
-
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         statusCheck()
         webView = findViewById(R.id.webView)
         val url = "https://maxelint.com/"
@@ -75,18 +86,83 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buildAlertMessageNoGps() {
-        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-            .setCancelable(false)
-            .setPositiveButton("Yes",
-                DialogInterface.OnClickListener { dialog, id -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) })
-            .setNegativeButton("No",
-                DialogInterface.OnClickListener { dialog, id -> dialog.cancel()
-                    this.finish();
-                    System.exit(0);
-                })
-        val alert: AlertDialog = builder.create()
-        alert.show()
+        if(!isGpsEnabled()) {
+            val locationRequest = LocationRequest.create()
+            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+            builder.setAlwaysShow(true) //this displays dialog box like Google Maps with two buttons - OK and NO,THANKS
+
+
+            val task =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build())
+            task.addOnCompleteListener { task ->
+                try {
+                    val response = task.getResult(ApiException::class.java)
+                    // All location settings are satisfied. The client can initialize location
+                    // requests here.
+                } catch (exception: ApiException) {
+                    when (exception.statusCode) {
+                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->                             // Location settings are not satisfied. But could be fixed by showing the
+                            // user a dialog.
+                            try {
+                                // Cast to a resolvable exception.
+                                val resolvable = exception as ResolvableApiException
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                resolvable.startResolutionForResult(
+                                    this@MainActivity,
+                                    REQUEST_CHECK_SETTINGS
+                                )
+                            } catch (e: SendIntentException) {
+                                // Ignore the error.
+                            } catch (e: ClassCastException) {
+                                // Ignore, should be an impossible error.
+                            }
+                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                        }
+                    }
+                }
+            }
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "GPS is already Enabled!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val states = LocationSettingsStates.fromIntent(data)
+        when (requestCode) {
+            REQUEST_CHECK_SETTINGS -> when (resultCode) {
+                RESULT_OK ->
+                    // All required changes were successfully made
+
+                    Toast.makeText(
+                        applicationContext,
+                        "User has clicked on OK - So GPS is on",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                RESULT_CANCELED ->closeApp()
+                // The user was asked to change settings, but chose not to
+
+
+                else -> {
+
+                }
+            }
+        }
+    }
+
+    private fun closeApp() {
+        Toast.makeText(
+            applicationContext,
+            "User has clicked on NO, THANKS - So GPS is still off.",
+            Toast.LENGTH_SHORT
+        ).show()
+        this.finishAffinity();
+        System.exit(0)
     }
 }
 
